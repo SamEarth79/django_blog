61a87bddaf71727f7359803567fd2bb338be9551
Samarth M <samarthmm7@gmail.com>
2023-03-30 13:35:44 +0530

    added home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+posts = [
+    {
+        'title': 'My favourite games',
+        'author': 'Samarth M',
+    },
+    {
+        'title': 'Top 10 games',
+        'author': 'Samarth M',
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
 # Create your views here.
